e3100a63903834844bcb557972c07bdb9dabcf6d
_caseyhunt_ <>
2021-07-14 11:57:36 -0600

    adding the basic outline :)

---
+++ index.js
@@ -0,0 +1,246 @@
+
+const CUBE_ID_ARRAY = [ 0, 1, 2 ];
+const SUPPORT_CUBE_NUM = CUBE_ID_ARRAY.length;
+
+// Global Variables.
+const gCubes = [ undefined, undefined, undefined ];
+
+
+
+
+
+  const SERVICE_UUID              = '10b20100-5b3b-4571-9508-cf3efcd7bbae';
+  const MOVE_CHARCTERISTICS_UUID = '10b20102-5b3b-4571-9508-cf3efcd7bbae';
+  const SOUND_CHARCTERISTICS_UUID = '10b20104-5b3b-4571-9508-cf3efcd7bbae';
+  const LIGHT_CHARCTERISTICS_UUID = '10b20103-5b3b-4571-9508-cf3efcd7bbae';
+
+  const connectNewCube = () => {
+
+      const cube = {
+          device:undefined,
+          sever:undefined,
+          service:undefined,
+          soundChar:undefined,
+          moveChar:undefined,
+          lightChar:undefined
+
+      };
+
+      // Scan only toio Core Cubes
+      const options = {
+          filters: [
+              { services: [ SERVICE_UUID ] },
+          ],
+      }
+
+      navigator.bluetooth.requestDevice( options ).then( device => {
+          cube.device = device;
+          if( cube === gCubes[0] ){
+              turnOnLightCian( cube );
+
+              const cubeID = 1;
+              changeConnectCubeButtonStatus( cubeID, undefined, true );
+          }else if( cube === gCubes[1] ){
+              turnOnLightGreen( cube );
+              spinCube( cube );
+              const cubeID = 2;
+              changeConnectCubeButtonStatus( cubeID, undefined, true );
+          }
+          changeConnectCubeButtonStatus( undefined, cube, false );
+          return device.gatt.connect();
+      }).then( server => {
+          cube.server = server;
+          return server.getPrimaryService( SERVICE_UUID );
+      }).then(service => {
+          cube.service = service;
+          return cube.service.getCharacteristic( MOVE_CHARCTERISTICS_UUID );
+      }).then( characteristic => {
+          cube.moveChar = characteristic;
+          return cube.service.getCharacteristic( SOUND_CHARCTERISTICS_UUID );
+      }).then( characteristic => {
+          cube.soundChar = characteristic;
+          return cube.service.getCharacteristic( LIGHT_CHARCTERISTICS_UUID );
+      }).then( characteristic => {
+          cube.lightChar = characteristic;
+          if( cube === gCubes[0] ){
+            turnOnLightCian( cube );
+            spinCube( cube );
+            enableMoveButtons();
+          }else if( cube === gCubes[1] ){
+            turnOnLightGreen( cube );
+          }else{
+            turnOnLightRed( cube );
+          }
+      });
+
+      return cube;
+  }
+
+
+
+  // Cube Commands
+  // -- Light Commands
+  const turnOffLight = ( cube ) => {
+
+      const CMD_TURN_OFF = 0x01;
+      const buf = new Uint8Array([ CMD_TURN_OFF ]);
+      if( ( cube !== undefined ) && ( cube.lightChar !== undefined ) ){
+          cube.lightChar.writeValue( buf );
+      }
+
+  }
+
+
+  const turnOnLightGreen = ( cube ) => {
+
+      // Green light
+      const buf = new Uint8Array([ 0x03, 0x00, 0x01, 0x01, 0x00, 0xFF, 0xFF]);
+      if( ( cube !== undefined ) && ( cube.lightChar !== undefined ) ){
+          cube.lightChar.writeValue( buf );
+          console.log('green');
+      }
+
+  }
+
+  const turnOnLightCian = ( cube ) => {
+
+      // Cian light
+    const buf = new Uint8Array([ 0x03, 0x00, 0x01, 0x01, 0x00, 0xFF, 0xFF ]);
+      if( ( cube !== undefined ) && ( cube.lightChar !== undefined ) ){
+          cube.lightChar.writeValue( buf );
+          console.log('cyan');
+
+      }
+
+  }
+
+  const turnOnLightRed = ( cube ) => {
+
+      // Red light
+      const buf = new Uint8Array([ 0x03, 0x00, 0x01, 0x01, 0xFF, 0x00, 0x00 ]);
+      if( ( cube !== undefined ) && ( cube.lightChar !== undefined ) ){
+          cube.lightChar.writeValue( buf );
+      }
+
+  }
+
+
+  const spinCube = ( cube ) => {
+
+      // Green light
+      const buf = new Uint8Array([ 0x02, 0x01, 0x01, 0x64, 0x02, 0x02, 0x14, 0x64 ]);
+      if( ( cube !== undefined ) && ( cube.moveChar !== undefined ) ){
+          cube.moveChar.writeValue( buf );
+          console.log('spin');
+      }
+
+  }
+
+  const changeButtonStatus = ( btID, enabled ) => {
+      document.getElementById( btID ).disabled = !enabled;
+  }
+
+
+  const changeConnectCubeButtonStatus = ( idButton, cube, enabled ) => {
+
+      if( idButton ){
+          changeButtonStatus( 'btConnectCube' + ( idButton + 1 ), enabled );
+      }else{
+          if( gCubes[0] === cube ){
+              changeButtonStatus( 'btConnectCube1', enabled );
+          }else if( gCubes[1] === cube ){
+              changeButtonStatus( 'btConnectCube2', enabled );
+          }else{
+              changeButtonStatus( 'btConnectCube3', enabled );
+          }
+      }
+
+  }
+
+  const enableMoveButtons = () => {
+    changeButtonStatus( 'btMoveFW', true );
+    changeButtonStatus( 'btMoveB', true );
+    changeButtonStatus( 'btMoveL', true );
+    changeButtonStatus( 'btMoveR', true );
+  }
+
+
+  const cubeMove = ( moveID ) => {
+      const cube = gCubes[0];
+      var buf = new Uint8Array([ 0x01, 0x01, 0x01, 0x64, 0x02, 0x01, 0x64]);
+      console.log(moveID);
+      // forward
+      if(moveID==1){
+      buf = new Uint8Array([ 0x01, 0x01, 0x01, 0x64, 0x02, 0x01, 0x64]);
+    }else if (moveID==2){
+      buf = new Uint8Array([ 0x01, 0x01, 0x02, 0x64, 0x02, 0x02, 0x64]);
+    }else if (moveID==3){
+      buf = new Uint8Array([ 0x01, 0x01, 0x02, 0x14, 0x02, 0x01, 0x64]);
+    }else if (moveID==4){
+      buf = new Uint8Array([ 0x01, 0x01, 0x01, 0x64, 0x02, 0x02, 0x14]);
+    }
+      if( ( cube !== undefined ) && ( cube.moveChar !== undefined ) ){
+          cube.moveChar.writeValue( buf );
+          console.log('move');
+      }
+
+  }
+
+  const cubeStop = () =>{
+      const cube = gCubes[0];
+      const buf = new Uint8Array([ 0x01, 0x01, 0x01, 0x00, 0x02, 0x01, 0x00]);
+      if( ( cube !== undefined ) && ( cube.moveChar !== undefined ) ){
+          cube.moveChar.writeValue( buf );
+          console.log('stop');
+      }
+  }
+
+  const initialize = () => {
+
+    // Event Listning for GUI buttons.
+    for( let cubeId of CUBE_ID_ARRAY ){
+        document.getElementById( 'btConnectCube' + ( cubeId + 1) ).addEventListener( 'click', async ev => {
+
+            if( cubeId === 0 ){
+                gCubes[0] = connectNewCube();
+                console.log('cube 0 connected (cyan)');
+            }else if( cubeId === 1 ){
+                gCubes[1] = connectNewCube();
+                console.log('cube 1 connected (green)');
+            }else{
+                gCubes[2] = connectNewCube();
+                console.log('cube 3 connected (red)');
+            }
+
+          });
+      }
+
+      document.getElementById( 'btMoveFW' ).addEventListener( 'mousedown', async ev => {
+        cubeMove( 1 );
+      });
+
+      document.getElementById( 'btMoveFW' ).addEventListener( 'mouseup', async ev => {
+        cubeStop();
+      });
+      document.getElementById( 'btMoveB' ).addEventListener( 'mousedown', async ev => {
+        cubeMove( 2 );
+      });
+      document.getElementById( 'btMoveB' ).addEventListener( 'mouseup', async ev => {
+        cubeStop();
+      });
+      document.getElementById( 'btMoveL' ).addEventListener( 'mousedown', async ev => {
+        cubeMove( 3 );
+      });
+      document.getElementById( 'btMoveL' ).addEventListener( 'mouseup', async ev => {
+        cubeStop();
+      });
+      document.getElementById( 'btMoveR' ).addEventListener( 'mousedown', async ev => {
+        cubeMove( 4 );
+      });
+      document.getElementById( 'btMoveR' ).addEventListener( 'mouseup', async ev => {
+        cubeStop();
+      });
+
+  }
+
+  initialize();
